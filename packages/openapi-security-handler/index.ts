21c8f3b271878c992c9e6c2864789deec045cce8
medfreeman <mlahlou@protonmail.ch>
2020-09-22 12:35:15 +0200

    Revert "feat(openapi-security-handler): rethrow error if it is not an instance of an error, has no `statusCode` or `statusCode` is equal or greater than 500"
    This reverts commit 9e31b41e1bf88c0822087e789586133e650eb9ed.

--- packages/openapi-security-handler/index.ts
+++ packages/openapi-security-handler/index.ts
@@ -147,13 +147,6 @@ export default class OpenAPISecurityHandler implements IOpenAPISecurityHandler {
 
           return Array.prototype.concat(resolvedChain, [result]);
         } catch (err) {
-          if (
-            !(err instanceof Error) ||
-            !('statusCode' in err) ||
-            err['statusCode'] >= 500
-          ) {
-            throw err;
-          }
           return Array.prototype.concat(resolvedChain, [err as Error]);
         }
       },
